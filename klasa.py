26b4e32dfd04e28a735eb39a2846764ea0b83d72
Wojtek Turek <s21611@pjwstk.edu.pl>
2019-12-14 13:44:16 +0100

    klasa

---
+++ klasa.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+class Klasa:
+
+    def __init__(self, x):
+        self.x = x
+
+    def string(self, s):
+        return self.x.join(s)
+
+k = Klasa('\n')
+print(k.string(sys.argv))
